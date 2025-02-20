9a1bc420dc32b7f2b0c7398e5455ddcd1b6fa528
itsalexi <108108603+itsalexi@users.noreply.github.com>
2025-02-20 14:08:13 +0800

    merge with kens branch

--- Game.java
+++ Game.java
@@ -4,7 +4,6 @@ public class Game {
   public static void main(String[] args) {
     int w = 640;
     int h = 480;
-    final int FPS = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getRefreshRate();
     JFrame f = new JFrame();
     GameCanvas gc = new GameCanvas(w, h);
 
