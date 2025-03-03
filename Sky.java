3a70948805bac36b84526c342b0a4dfe3ce93813
alexi <108108603+itsalexi@users.noreply.github.com>
2025-03-03 22:02:39 +0800

    add more cloud positions

--- Sky.java
+++ Sky.java
@@ -9,8 +9,23 @@ public class Sky extends ParallaxObject {
     objects = new ArrayList<>();
 
     objects.add(new Cloud(50, 100, 0));
-    objects.add(new Cloud(100, 125, 0));
-    objects.add(new Cloud(250, 135, 1));
+    objects.add(new Cloud(60, 127, 1));
+    objects.add(new Cloud(100, 80, 0));
+    objects.add(new Cloud(250, 98, 1));
+    objects.add(new Cloud(320, 115, 0));
+    objects.add(new Cloud(350, 110, 0));
+    objects.add(new Cloud(310, 90, 1));
+    objects.add(new Cloud(432, 135, 1));
+    objects.add(new Cloud(475, 100, 0));
+    objects.add(new Cloud(550, 115, 0));
+    objects.add(new Cloud(585, 105, 1));
+    objects.add(new Cloud(640, 136, 0));
+    objects.add(new Cloud(625, 126, 1));
+    objects.add(new Cloud(686, 115, 1));
+    objects.add(new Cloud(712, 165, 0));
+    objects.add(new Cloud(743, 126, 1));
+
+    objects.add(new Cloud(800, 110, 1));
 
   }
 
